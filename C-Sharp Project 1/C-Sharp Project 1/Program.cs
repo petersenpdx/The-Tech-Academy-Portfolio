8012578408b1b8af2f39e9e12516b18a03278e1a
Mark Petersen <petersenpdx@gmail.com>
2018-11-28 21:12:49 -0900

    C# Excercise on page 34, Acme Accounting Page

---
+++ C-Sharp Project 1/C-Sharp Project 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace C_Sharp_Project_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accouning Systems");
+            Console.ReadLine();
+            Console.WriteLine("Remember we're \"accounting\" on you");
+            Console.ReadLine();
+        }
+    }
+}
