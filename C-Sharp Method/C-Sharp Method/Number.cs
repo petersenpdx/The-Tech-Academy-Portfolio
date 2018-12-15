3e5c2ecf9d5778770e2d974ff6eb4590208faa7d
Mark Petersen <petersenpdx@gmail.com>
2018-12-14 18:49:20 -0900

    C# Method (Calling Operation Function)
    C# and .NET Framework, page 112

---
+++ C-Sharp Method/C-Sharp Method/Number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C_Sharp_Method
+{
+ public class Number
+ {    
+       public int Sum(int num1, int num2)
+    {
+        
+        int total;
+        total = num1 + num2;
+        return total;
+    }
+        public int Subtract(int num1, int num2)
+        {
+            int total;
+            total = num1 - num2;
+            return total;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            int total;
+            total = num1 * num2;
+            return total;
+        }
+    }
+}
---
+++ C-Sharp Methodes Game/C-Sharp Methodes Game/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//EXERCISE:
+//IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+//1. Create a class. In that class, create three methods, each of which will take one integer parameter in and return an 
+//    integer.The methods should do some math operation on the received parameter.Put this class in a separate.cs file in the application.
+//2. In the Main() program, ask the user what number they want to do the math operations on.
+//3. Call each method in turn, passing the user input to the method.Display the returned integer to the screen.
+
+namespace C_Sharp_Methodes_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck = Shuffle(deck);
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
+
+        }
+    }
+}
