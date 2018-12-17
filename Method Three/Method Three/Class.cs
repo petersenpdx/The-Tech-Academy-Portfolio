bc6929e091c23150b3e28ef76f024303e141ffa4
Mark Petersen <petersenpdx@gmail.com>
2018-12-17 11:51:25 -0900

    C# Three Method - Math Operations
    C# and .NET Framework, page 114

---
+++ Method Three/Method Three/Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Three
+{
+    public class Class
+    {
+        
+        public int The(int num1)
+        {
+            int total = num1 + 3;
+            Console.WriteLine(total);
+            return total;
+        }
+
+        public decimal Dec(decimal num1)
+        {
+            decimal total = num1 * 3.3m;
+            Console.WriteLine(total);
+            return total;
+        }
+
+        public void Word(string numString)
+        {
+
+            int Num = Convert.ToInt16(numString);
+            int total = 20 - Num;
+            Console.WriteLine(total);
+            return;
+        }
+
+    }
+}
---
+++ Method Three/Method Three/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//EXERCISE:
+//IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+//1. Create a class. In that class, create a method that will take in an integer, 
+//do a math operation to it and then return the answer as an integer.
+//2. In the Main() method of the console app, instantiate the class and call the
+//one method, passing in an integer, such as 12. Then display the result to the screen.
+//3. Add a second method to the class, with the same name, that will take in a decimal, 
+//do a different math operation to it and then return the answer as an integer.
+//4. In the Main() method of the console app, instantiate the class and call the second 
+//method, passing in a decimal. Display the result to the screen.
+//5. Add a third method to the class, with the same name, that will take in a string, convert
+//it to an integer if possible, do a different math operation to it and then return the answer as an integer.
+//6. In the Main() method of the console app, instantiate the class and call the third method, passing 
+//in a string that equates to an integer.Display the result to the screen.
+
+namespace Method_Three
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int Number = 12;
+            Method_Three.Class Addition = new Class();
+            {
+                Addition.The(Number);
+                Console.ReadLine();
+
+                Addition.Dec(Number);
+                Console.ReadLine();
+
+                string String = "12";
+                Addition.Word(String);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
