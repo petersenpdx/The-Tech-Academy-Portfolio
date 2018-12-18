e09f3b28128cf391f011e7d1cbe373d817eeda13
Mark Petersen <petersenpdx@gmail.com>
2018-12-18 12:21:21 -0900

    C# Overload Operator upload
    C# and .NET Framework, page 129

---
+++ Overload Operator/Overload Operator/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator
+{
+    
+        public class Employee : Person, IQuittable
+        {
+            public int id;
+            public void Number()
+            {
+                id = 0;
+            }
+            public void Number(int x)
+            {
+                id = x;
+            }
+            public static bool operator ==(Employee x, Employee y)
+            {
+                if (x.id == y.id)
+                {
+                    Console.WriteLine("== yes");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("== no");
+                    return false;
+                }
+            }
+            public static bool operator !=(Employee x, Employee y)
+            {
+                if (x.id != y.id)
+                {
+                    Console.WriteLine("!= yes");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("!= no");
+                    return false;
+                }
+            }
+            public int getValue()
+            {
+                return id;
+            }
+
+            public override void SayName()
+            {
+                Console.WriteLine("First Name: " + firstName);
+                Console.WriteLine("Last Name: " + lastName);
+            }
+            public void Quit()
+            {
+                Console.WriteLine("Quit.");
+            }
+        }
+}
