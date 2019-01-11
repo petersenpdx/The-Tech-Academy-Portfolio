6babba142c3869093c1a35ac7ada3166b89e24f9
Mark Petersen <petersenpdx@gmail.com>
2019-01-10 20:56:07 -0900

    Update

---
+++ Quote/Quote/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quote.ViewModels;
+
+namespace Quote.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (QuotesEntities db = new QuotesEntities())
+            {
+
+                var quotes = db.Quotes.Where(x => x.Removed == null).ToList();
+                var QuoteVms = new List<QuoteVm>();
+                foreach (var Quotes in QuoteVms)
+                {
+
+                    //var is named "Quotes" rather than "Quote" because the title
+                    //of the edmx table couldn't be the same as the parameter "Quote".
+                    var QuoteVm = new QuoteVm();
+                    QuoteVm.Id = Quotes.Id;
+                    QuoteVm.Quote = Convert.ToDouble(Quotes.Quote);
+                    QuoteVm.FirstName = Quotes.FirstName;
+                    QuoteVm.LastName = Quotes.LastName;
+                    QuoteVm.EmailAddress = Quotes.EmailAddress;
+                    QuoteVms.Add(QuoteVm);
+                }
+                return View(QuoteVms);
+            }
+        }
+
+        public ActionResult Remove(int Id)
+        {
+            using (QuotesEntities db = new QuotesEntities())
+            {
+                var quote = db.Quotes.Find(Id);
+                quote.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Quote/Quote/Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quote.Models;
+using Quote.ViewModels;
+
+namespace Quote.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Quotes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Quote(string FirstName, string LastName, string EmailAddress, string DateOfBirth, string CarYear, string CarMake, string CarModel, string SpeedingTickets, string DUI, string Coverage)
+        {
+            if (string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName) || string.IsNullOrEmpty(EmailAddress) || string.IsNullOrEmpty(DateOfBirth) || string.IsNullOrEmpty(CarYear) || string.IsNullOrEmpty(CarMake) || string.IsNullOrEmpty(CarModel) || string.IsNullOrEmpty(SpeedingTickets) || string.IsNullOrEmpty(DUI) || string.IsNullOrEmpty(Coverage))
+
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+
+            else
+            {
+
+                DateTime Dob = Convert.ToDateTime(DateOfBirth);
+                int age = 0;
+                age = DateTime.Now.Year - Dob.Year;
+                if (DateTime.Now.DayOfYear < Dob.DayOfYear)
+                    age = age - 1;
+                double total = 50;
+
+
+                bool under25orOver100 = age < 25 || age > 100;
+                if (under25orOver100)
+                {
+                    total = total + 25;
+                }
+
+                bool under18 = age < 18;
+                if (under18)
+                {
+
+                    total = total + 100;
+                }
+
+                else
+                {
+                    total = total;
+                }
+
+
+                int carYear = Convert.ToInt32(CarYear);
+
+
+                if (carYear < 2000 || carYear > 2015)
+                {
+                    total = total + 25;
+                }
+
+                else
+                {
+                    total = total;
+                }
+
+                if (CarMake == "porsche" || CarMake == "Porsche")
+                {
+                    total = total + 25;
+                }
+                else
+                {
+                    total = total;
+                }
+                if (CarMake == "porsche" && CarModel == "911 Carrera" || CarMake == "Porsche" && CarModel == "911 Carrera")
+                {
+                    total = total + 25;
+                }
+                else
+                {
+                    total = total;
+                }
+
+                int tickets = Convert.ToInt32(SpeedingTickets);
+
+                int ticketTotal = tickets * 10;
+
+                total = total + ticketTotal;
+
+                bool Dui = DUI == "yes" || DUI == "Yes";
+                if (Dui)
+                {
+                    total = total + (total * .25);
+                }
+                else
+                {
+                    total = total;
+                }
+
+                bool FullCoverage = Coverage == "full coverage" || Coverage == "full" || Coverage == "Full Coverage" || Coverage == "Full";
+
+                if (FullCoverage)
+                {
+                    total = total + (total * .5);
+                }
+                else
+                {
+                    total = total;
+                }
+
+                double Quote = total;
+
+                ViewBag.Quote = "$" + Quote;
+
+                using (QuotesEntities db = new QuotesEntities())
+                {
+                    var Quotes = new Quotes();
+                    Quotes.FirstName = FirstName;
+                    Quotes.LastName = LastName;
+                    Quotes.EmailAddress = EmailAddress;
+                    string Quote1 = Convert.ToString(Quote);
+                    Quotes.Quote = Quote1;
+
+                    db.Quotes.Add(Quotes);
+                    db.SaveChanges();
+                }
+                return View("Success");
+            }
+        }
+    }
+}
